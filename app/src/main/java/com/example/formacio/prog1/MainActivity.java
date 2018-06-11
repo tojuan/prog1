8ba8557e79f85489d0e331dba02b6cad2a5daddb
M022663\formacio <jasbustamante@hotmail.com>
2018-06-11 12:40:03 +0200

    mod

--- app/src/main/java/com/example/formacio/prog1/MainActivity.java
+++ app/src/main/java/com/example/formacio/prog1/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
+//test
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
